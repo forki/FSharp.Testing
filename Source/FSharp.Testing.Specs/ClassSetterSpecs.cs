0d85e2a7f339c02c3829f3682d4cc2c3c44172dd
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2011-03-01 14:29:18 +0100

    Splitting Specs for Records and Classes

---
+++ Source/FSharp.Testing.Specs/ClassSetterSpecs.cs
@@ -0,0 +1,24 @@
+﻿using Machine.Specifications;
+
+namespace FSharp.Testing.Specs
+{
+    class MyClass
+    {
+        public MyClass(int property1)
+        {
+            Property1 = property1;
+        }
+
+        public int Property1 { get; private set; }
+    }
+
+    public class when_setting_a_value_on_an_automatic_property_with_no_visible_setter_in_the_class
+    {
+        static MyClass _myClass;
+        Establish context = () => _myClass = new MyClass(0);
+
+        Because of = () => _myClass.Set(x => x.Property1).To(10);
+
+        It should_reflect_the_new_value = () => _myClass.Property1.ShouldEqual(10);
+    }
+}
--- Source/FSharp.Testing.Specs/RecordSetterSpecs.cs
+++ Source/FSharp.Testing.Specs/RecordSetterSpecs.cs
@@ -3,30 +3,10 @@ using Machine.Specifications;
 
 namespace FSharp.Testing.Specs
 {
-    public class when_setting_a_value_on_an_automatic_property_with_no_visible_setter_in_the_class
-    {
-        static MyClass _myClass;
-        Establish context = () => _myClass = new MyClass(0);
-
-        Because of = () => _myClass.Set(x => x.Property1).To(10);
-
-        It should_reflect_the_new_value = () => _myClass.Property1.ShouldEqual(10);
-    }
-
-    class MyClass
-    {
-        public MyClass(int property1)
-        {
-            Property1 = property1;
-        }
-
-        public int Property1 { get; private set; }
-    }
-
     public class when_setting_a_value_on_an_automatic_property_with_no_visible_setter_in_the_record
     {
         static Records.MyRecord _myClass;
-        Establish context = () => _myClass = new Records.MyRecord(0,"");
+        Establish context = () => _myClass = new Records.MyRecord(0, "");
 
         Because of = () => _myClass.Set(x => x.Property1).To(10);
 
