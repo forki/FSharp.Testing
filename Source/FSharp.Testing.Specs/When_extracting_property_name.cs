2cdb90d7cdf329a3b1dc58cadafe43b5f5c8598f
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2011-03-01 14:12:16 +0100

    Fixing wording in spec

--- Source/FSharp.Testing.Specs/When_extracting_property_name.cs
+++ Source/FSharp.Testing.Specs/When_extracting_property_name.cs
@@ -2,14 +2,14 @@
 
 namespace FSharp.Testing.Specs
 {
-    public class When_extracting_property_name
+    public class when_setting_a_value_on_an_automatic_property_with_no_visible_setter
     {
         static MyRecord _myRecord;
         Establish context = () => _myRecord = new MyRecord(0);
 
         Because of = () => _myRecord.Set(x => x.Property1).To(10);
 
-        It should_have_right_value_in_property = () => _myRecord.Property1.ShouldEqual(10);
+        It should_reflect_the_new_value = () => _myRecord.Property1.ShouldEqual(10);
     }
 
     class MyRecord
