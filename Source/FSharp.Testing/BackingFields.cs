a463c3fa3d63a72f722624fd71803a3535f38b2a
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2011-03-02 11:24:55 +0100

    Cleanup

---
+++ Source/FSharp.Testing/BackingFields.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Reflection;
+
+namespace FSharp.Testing
+{
+    public class BackingFields
+    {
+        /// <summary>
+        ///   Gets the backing field for the given property.
+        /// </summary>
+        /// <typeparam name = "T"></typeparam>
+        /// <param name = "propertyName">Name of the property.</param>
+        /// <returns></returns>
+        public static FieldInfo GetBackingField<T>(string propertyName)
+        {
+            return GetBackingField(typeof (T), propertyName);
+        }
+
+        /// <summary>
+        /// Gets the backing field for a given property name.
+        /// </summary>
+        /// <param name="type">The type.</param>
+        /// <param name="propertyName">Name of the property.</param>
+        /// <returns></returns>
+        public static FieldInfo GetBackingField(Type type, string propertyName)
+        {
+            // TODO: Apply Open-Closed-Principle
+            var fieldName = propertyName + "@";
+            var field = GetPrivateFieldInfo(type, fieldName);
+
+            if (field == null)
+            {
+                fieldName = String.Format("<{0}>k__BackingField", propertyName);
+                field = GetPrivateFieldInfo(type, fieldName);
+            }
+
+            if (field == null)
+            {
+                fieldName = propertyName + "Field";
+                field = GetPrivateFieldInfo(type, fieldName);
+            }
+            if (field == null)
+                throw new Exception(String.Format("Backing field for property {0} could not be found.", propertyName));
+            return field;
+        }
+
+        static FieldInfo GetPrivateFieldInfo(Type type, string fieldName)
+        {
+            return type.GetField(fieldName, BindingFlags.NonPublic | BindingFlags.Instance);
+        }
+    }
+}
--- Source/FSharp.Testing/ReflectionExtensions.cs
+++ Source/FSharp.Testing/ReflectionExtensions.cs
@@ -10,50 +10,6 @@ namespace FSharp.Testing
     public static class ReflectionExtensions
     {
         /// <summary>
-        ///   Gets the backing field for the given property.
-        /// </summary>
-        /// <typeparam name = "T"></typeparam>
-        /// <param name = "propertyName">Name of the property.</param>
-        /// <returns></returns>
-        public static FieldInfo GetBackingField<T>(string propertyName)
-        {
-            return GetBackingField(typeof (T), propertyName);
-        }
-
-        /// <summary>
-        /// Gets the backing field for a given property name.
-        /// </summary>
-        /// <param name="type">The type.</param>
-        /// <param name="propertyName">Name of the property.</param>
-        /// <returns></returns>
-        public static FieldInfo GetBackingField(Type type, string propertyName)
-        {
-            // TODO: Apply Open-Closed-Principle
-            var fieldName = propertyName + "@";
-            var field = GetPrivateFieldInfo(type, fieldName);
-
-            if (field == null)
-            {
-                fieldName = string.Format("<{0}>k__BackingField", propertyName);
-                field = GetPrivateFieldInfo(type, fieldName);
-            }
-
-            if (field == null)
-            {
-                fieldName = propertyName + "Field";
-                field = GetPrivateFieldInfo(type, fieldName);
-            }
-            if (field == null)
-                throw new Exception(string.Format("Backing field for property {0} could not be found.", propertyName));
-            return field;
-        }
-
-        static FieldInfo GetPrivateFieldInfo(Type type, string fieldName)
-        {
-            return type.GetField(fieldName, BindingFlags.NonPublic | BindingFlags.Instance);
-        }
-
-        /// <summary>
         ///   Gets the name of the property.
         /// </summary>
         /// <typeparam name = "T"></typeparam>
